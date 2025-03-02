8a20852f54590ed4a24137128d629f5c3d74f1b2
dagon1093 <dagon1093@gmail.com>
2025-03-03 00:32:58 +0300

    Добавлен интерфейс

---
+++ console-todo-manager/Service/IToDoService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleToDoManager.Models;
+
+namespace ConsoleToDoManager.Service
+{
+    internal interface IToDoService
+    {
+        public void AddTask(string title);
+        public ToDoTask GetTaskByTitle(string title);
+        public void DeleteTask(string title);
+        public void DeleteTaskById(int id);
+        public List<ToDoTask> ListTasks();
+        public void CompleteTask(string title);
+        public void EditTask(int id, string? newTitle);
+        public void ShowTasks(List<ToDoTask> tasks);
+
+    }
+}
